bc7a3dfe83b097155f09096a096fec412f6ea993
idonag <idonag@post.bgu.ac.il>
2024-02-03 20:44:35 +0200

    try sqs pull push

--- src/main/java/Local.java
+++ src/main/java/Local.java
@@ -1,9 +1,10 @@
 public class Local {
     public static void main(String[] args) {
-        String amid = "ami-06f533aa95c8c0dad";
+        String amid = "ami-0b407295341ecbda3";
         AWSHandler awsHandler = new AWSHandler();
         awsHandler.createSqs("inputs");
         awsHandler.createSqs("outputs");
-        awsHandler.createEC2Instance("wget manager-jar-url\nwget input-file\njava -jar manager.jar input-file","manager",amid,1);
+        awsHandler.createEC2Instance("wget https://workerbucketido.s3.amazonaws.com/manager.jar\nwget https://workerbucketido.s3.amazonaws.com/input1.txt","manager",amid,1);
     }
+    //java -jar manager.jar input1.txt
 }
